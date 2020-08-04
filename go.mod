f9983356563fed0640f5abab8db89cd41808136b
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-08-04 22:14:39 +0530

    minor

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module protobuf_using_go
 
 go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.2
+	google.golang.org/protobuf v1.23.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+)
+
 func main() {
 
+	animal := &Book{
+		Name: "Animal Farm",
+		Isbn: 104,
+	}
+
+	data, err := proto.Marshal(animal)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+
+	// printing out our raw protobuf object
+	fmt.Println(data)
+
+	// let's go the other way and unmarshal
+	// our byte array into an object we can modify
+	// and use
 }
