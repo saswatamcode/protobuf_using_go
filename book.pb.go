27428a44f0de9d3cb66b0ad0524692ac2d861427
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-08-05 09:34:42 +0530

    Add nested Fields

--- book.pb.go
+++ book.pb.go
@@ -25,6 +25,61 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
+type Author struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Name             string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	YearOfPublishing int32  `protobuf:"varint,2,opt,name=yearOfPublishing,proto3" json:"yearOfPublishing,omitempty"`
+}
+
+func (x *Author) Reset() {
+	*x = Author{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_book_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Author) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Author) ProtoMessage() {}
+
+func (x *Author) ProtoReflect() protoreflect.Message {
+	mi := &file_book_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Author.ProtoReflect.Descriptor instead.
+func (*Author) Descriptor() ([]byte, []int) {
+	return file_book_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *Author) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *Author) GetYearOfPublishing() int32 {
+	if x != nil {
+		return x.YearOfPublishing
+	}
+	return 0
+}
+
 type Book struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -32,12 +87,14 @@ type Book struct {
 
 	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
 	Isbn int32  `protobuf:"varint,2,opt,name=isbn,proto3" json:"isbn,omitempty"`
+	//nesting a field
+	Author *Author `protobuf:"bytes,3,opt,name=author,proto3" json:"author,omitempty"`
 }
 
 func (x *Book) Reset() {
 	*x = Book{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_book_proto_msgTypes[0]
+		mi := &file_book_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -50,7 +107,7 @@ func (x *Book) String() string {
 func (*Book) ProtoMessage() {}
 
 func (x *Book) ProtoReflect() protoreflect.Message {
-	mi := &file_book_proto_msgTypes[0]
+	mi := &file_book_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -63,7 +120,7 @@ func (x *Book) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Book.ProtoReflect.Descriptor instead.
 func (*Book) Descriptor() ([]byte, []int) {
-	return file_book_proto_rawDescGZIP(), []int{0}
+	return file_book_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *Book) GetName() string {
@@ -80,14 +137,28 @@ func (x *Book) GetIsbn() int32 {
 	return 0
 }
 
+func (x *Book) GetAuthor() *Author {
+	if x != nil {
+		return x.Author
+	}
+	return nil
+}
+
 var File_book_proto protoreflect.FileDescriptor
 
 var file_book_proto_rawDesc = []byte{
 	0x0a, 0x0a, 0x62, 0x6f, 0x6f, 0x6b, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x04, 0x6d, 0x61,
-	0x69, 0x6e, 0x22, 0x2e, 0x0a, 0x04, 0x42, 0x6f, 0x6f, 0x6b, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61,
-	0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x12,
-	0x0a, 0x04, 0x69, 0x73, 0x62, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x04, 0x69, 0x73,
-	0x62, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x69, 0x6e, 0x22, 0x48, 0x0a, 0x06, 0x41, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x12, 0x12, 0x0a, 0x04,
+	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65,
+	0x12, 0x2a, 0x0a, 0x10, 0x79, 0x65, 0x61, 0x72, 0x4f, 0x66, 0x50, 0x75, 0x62, 0x6c, 0x69, 0x73,
+	0x68, 0x69, 0x6e, 0x67, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x10, 0x79, 0x65, 0x61, 0x72,
+	0x4f, 0x66, 0x50, 0x75, 0x62, 0x6c, 0x69, 0x73, 0x68, 0x69, 0x6e, 0x67, 0x22, 0x54, 0x0a, 0x04,
+	0x42, 0x6f, 0x6f, 0x6b, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x69, 0x73, 0x62, 0x6e,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x04, 0x69, 0x73, 0x62, 0x6e, 0x12, 0x24, 0x0a, 0x06,
+	0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x0c, 0x2e, 0x6d,
+	0x61, 0x69, 0x6e, 0x2e, 0x41, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x52, 0x06, 0x61, 0x75, 0x74, 0x68,
+	0x6f, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -102,16 +173,18 @@ func file_book_proto_rawDescGZIP() []byte {
 	return file_book_proto_rawDescData
 }
 
-var file_book_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_book_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_book_proto_goTypes = []interface{}{
-	(*Book)(nil), // 0: main.Book
+	(*Author)(nil), // 0: main.Author
+	(*Book)(nil),   // 1: main.Book
 }
 var file_book_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	0, // 0: main.Book.author:type_name -> main.Author
+	1, // [1:1] is the sub-list for method output_type
+	1, // [1:1] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_book_proto_init() }
@@ -121,6 +194,18 @@ func file_book_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_book_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Author); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_book_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Book); i {
 			case 0:
 				return &v.state
@@ -139,7 +224,7 @@ func file_book_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_book_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
--- main.go
+++ main.go
@@ -12,8 +12,13 @@ func main() {
 	myBook := &Book{
 		Name: "Animal Farm",
 		Isbn: 104,
+		Author: &Author{
+			Name:             "George Orwell",
+			YearOfPublishing: 1945,
+		},
 	}
 
+	// marshalling protocol buffer data
 	data, err := proto.Marshal(myBook)
 	if err != nil {
 		log.Fatal("marshaling error: ", err)
@@ -22,12 +27,17 @@ func main() {
 	fmt.Println(data)
 
 	myNewBook := &Book{}
+
+	// unmarshalling encoded protocol buffer
 	err = proto.Unmarshal(data, myNewBook)
 	if err != nil {
 		log.Fatal("Unmarshaling error: ", err)
 	}
 
+	// accessing fields with getter methods
 	fmt.Println(myNewBook.GetName())
 	fmt.Println(myNewBook.GetIsbn())
+	fmt.Println(myNewBook.Author.GetName())
+	fmt.Println(myNewBook.Author.GetYearOfPublishing())
 
 }
